a441e87cdbbdfde5673688cdf2395cbd6474f7c0
cardo1011 <ricardomunoz4250@gmail.com>
2026-08-08 14:18:06 -0700

    Created async-await functions in views-index.js to wait for the socket to connect. I wanted to implement this because in order to be able to succesfully create a room if a player chooses to invite a friend

--- sockets/newConnections.js
+++ sockets/newConnections.js
@@ -4,10 +4,10 @@ const currentIO = io;
 let playerCount = 0;
 
 function newPlayerConnected() {
+  playerCount++;
   currentIO.on("connection", (socket) => {
     socket.on("newConnection", () => {
       //Acknowledge player has came online.
-      playerCount++;
       console.log(
         `=======\nPlayer_${playerCount} connected to server\n=======`
       );
