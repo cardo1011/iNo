9d18094feaad48bf674be913d6fc781ad55b7523
cardo1011 <ricardomunoz4250@gmail.com>
2026-08-14 14:53:37 -0700

    wrote code to fetch questions from api. Will refactor fetch to get the 10 questions as intended and to display them as desired for the game. newConnections.js and socketHandlers.js have changes that were not staged due to only changing them to verify the fetch was working as intended for now.

--- services/triviaService.js
+++ services/triviaService.js
@@ -1,3 +1,17 @@
 /* 
  This file should contain the code necessary to fetch trivia questions from an external API and process them in order for them to be ready to actually be used in game.
 */
+
+async function fetchQuestions() {
+  let questionResponse = await fetch(
+    "https://opentdb.com/api.php?amount=1&type=multiple"
+  );
+
+  let questionsResponseBody = await questionResponse.json();
+  //   console.log("Succes:", questionsResponseBody);
+
+  let questions = questionsResponseBody.results[0].question;
+  console.log(questions);
+}
+
+export default fetchQuestions;
