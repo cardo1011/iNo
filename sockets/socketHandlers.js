4dcaf872b3fca9edb04394fcc002790d93f0dccf
cardo1011 <ricardomunoz4250@gmail.com>
2026-08-14 13:02:16 -0700

    moved 'rooms' variable to global scope in order to be able to access it in the future

---
+++ sockets/socketHandlers.js
@@ -0,0 +1,32 @@
+import { io } from "../servers.js";
+
+let playerCount = 0;
+// Accessing the rooms Map provided by socket.io
+const rooms = io.of("/").adapter.rooms;
+
+function socketHandlers() {
+  io.on("connection", (socket) => {
+    //Acknowledge player has came online.
+    socket.on("newConnection", () => {
+      playerCount++;
+      console.log(
+        `=======\nPlayer_${playerCount} connected to server\n=======`
+      );
+    });
+    // Connects player to room in order to initialize the game and provides them their socket ID to have their friend join the same room.
+    socket.on("inviteFriend", (room) => {
+      socket.join(room);
+      socket.emit("sendRoom", room);
+    });
+
+    // Connects second player to
+    socket.on("joinFriend", (room) => {
+      // Verify the room provided by the players exists before allowing them to join the game
+      if (rooms.has(room)) {
+        socket.join(room);
+        io.to(room).emit("showAlert", `Your opponent has joined the game`);
+      }
+    });
+  });
+}
+export default socketHandlers;
