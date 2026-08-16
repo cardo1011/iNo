c0d1223b5b329e4d7e4b716ab6601f59aa80d320
cardo1011 <ricardomunoz4250@gmail.com>
2026-08-16 14:11:43 -0700

    Implemented of the APIs provided tokens to keep track of if a game session has run out of questions or not. gameManager just has a comment for now, no code

--- services/triviaService.js
+++ services/triviaService.js
@@ -2,16 +2,29 @@
  This file should contain the code necessary to fetch trivia questions from an external API and process them in order for them to be ready to actually be used in game.
 */
 
-async function fetchQuestions() {
-  let questionResponse = await fetch(
-    "https://opentdb.com/api.php?amount=1&type=multiple"
+async function fetchToken() {
+  // Need to consider when a session token will NOT return succesfully
+
+  let tokenRes = await fetch(
+    "https://opentdb.com/api_token.php?command=request"
   );
+  let tokenResBody = await tokenRes.json();
+
+  // console.log(tokenResBody);
+  return tokenResBody.token;
+}
+
+async function fetchQuestions() {
+  let token = await fetchToken();
+  // console.log("trying to fetch token before questions: ", token);
+  let url = `https://opentdb.com/api.php?amount=1&token=${token}`;
+
+  let questionResponse = await fetch(url);
 
   let questionsResponseBody = await questionResponse.json();
-  //   console.log("Succes:", questionsResponseBody);
 
-  let questions = questionsResponseBody.results[0].question;
-  console.log(questions);
+  console.log("questionsResponseBody: ", questionsResponseBody.results);
+  return questionsResponseBody.results;
 }
 
 export default fetchQuestions;
