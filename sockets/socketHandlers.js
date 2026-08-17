4bd921a9f470e13c25dff755c89635a253943904
cardo1011 <ricardomunoz4250@gmail.com>
2026-08-17 13:24:34 -0700

    refactored socketHandlers to only house function calls instead of whole socket events in order to keep code organized. Created gameInitiation to house the socket events that are needed to begin a game. Fixed bug in index.ejs that was trying to append an element to a parent element before the parent element was created.

--- sockets/socketHandlers.js
+++ sockets/socketHandlers.js
@@ -1,37 +1,22 @@
 import { io } from "../servers.js";
-import fetchQuestions from "../services/triviaService.js";
+import {
+  acknowledgePlayerConnectingToServer,
+  inviteFriend,
+  joinFriend,
+} from "./gameInitiation.js";
 
 let playerCount = 0;
-// Accessing the rooms Map provided by socket.io
-const rooms = io.of("/").adapter.rooms;
 
 function socketHandlers() {
   io.on("connection", (socket) => {
     //Acknowledge player has came online.
-    socket.on("newConnection", () => {
-      playerCount++;
-      console.log(
-        `=======\nPlayer_${playerCount} connected to server\n=======`
-      );
-    });
+    acknowledgePlayerConnectingToServer(playerCount, socket);
+
     // Connects player to room in order to initialize the game and provides them their socket ID to have their friend join the same room.
-    socket.on("inviteFriend", (room) => {
-      socket.join(room);
-      socket.emit("sendRoom", room);
-    });
+    inviteFriend(socket);
 
     // Connects second player to same room as their friend
-    socket.on("joinFriend", async (room) => {
-      // Verify the room provided by the players exists before allowing them to join the game
-      if (rooms.has(room)) {
-        socket.join(room);
-        io.to(room).emit("showAlert", `Your opponent has joined the game`);
-
-        // waits for the array of question objects before sending it over to the front end
-        let questionsArr = await fetchQuestions();
-        io.to(room).emit("renderQuestions", questionsArr);
-      }
-    });
+    joinFriend(socket);
   });
 }
 export default socketHandlers;
